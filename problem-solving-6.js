cde1f6b1a18a221ab4c993860230665c04cc9969
Nazmul Alam <shawonkst15@gmail.com>
2022-02-03 19:22:14 +0600

    add new two  file

---
+++ problem-solving-6.js
@@ -0,0 +1,13 @@
+//  const myNumber = -5;
+//  const output = Math.abs(myNumber);
+//  console.group(output);
+
+const myNumber = 22.6398;
+// const output= Math.ceil(myNumber);
+// const output= Math.floor(myNumber);
+
+// const output = Math.round(myNumber);
+
+const output = Math.random()*20;
+const rounded = Math.floor(output);
+console.log(rounded);
---
+++ problem-solving-7.js
@@ -0,0 +1,11 @@
+var first = 5 ;
+var second = 7;
+
+console.log(first,second);
+
+var temp = first;
+first = second;
+second =temp;
+console.log(first,second) ;
+
+
