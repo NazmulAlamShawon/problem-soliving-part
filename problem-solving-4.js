530d293c096f605f53b194c3d2efecdd20deca82
Nazmul Alam <shawonkst15@gmail.com>
2022-02-03 13:34:26 +0600

    add new file

--- problem-solving-4.js
+++ problem-solving-4.js
@@ -1,11 +1,13 @@
 const friendsAge = [
-    {name:  'nazmul', age: 32},
-    {name:  'rubel',  age: 31},
-    {name:  'sumon',  age: 29},
-    {name:  'romel',  age: 26}
+    {name: 'nazmul', age: 32},
+    {name: 'rubel',  age: 31},
+    {name: 'sumon',  age: 29},
+    {name: 'romel',  age: 26},
+    {name: 'shofic', age: 23},
     
 ];
 let lowestAge = friendsAge[0];
+
 for (const age of friendsAge){
  
     if(age.age < lowestAge.age){
