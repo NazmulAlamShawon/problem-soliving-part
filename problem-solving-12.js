9aed6fd4961f4d93985d5f66a41f7280b109b896
Nazmul Alam <shawonkst15@gmail.com>
2022-02-04 09:47:17 +0600

    add new array

---
+++ problem-solving-12.js
@@ -0,0 +1,9 @@
+var friendAge = [11, 13, 17,18,19];
+
+var muriChanacurFee = [34,55,221,3];
+
+var naykas = ['popi', 'mahi','purnima', 'opu'];
+
+var vowels = ['a','e','i','o','u'];
+console.log(vowels.length);
+console.log(vowels);
